73a12c6ecfe59c4707ed098485414a5c70a528f0
ItPhantom <hw61102@gmail.com>
2016-02-20 15:29:45 +0900

    ㅋㅋ

--- app/src/main/java/flash/lucida/com/flash/MainActivity.java
+++ app/src/main/java/flash/lucida/com/flash/MainActivity.java
@@ -1,8 +1,11 @@
 package flash.lucida.com.flash;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_CAMERA = 2;
     private Switch swc; // 스위치 객체
     private Switch swc2; // 스위치 객체
     private TextView textView; // 스위치 객체
@@ -26,6 +30,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView= (TextView)findViewById(R.id.textView);
         swc = (Switch)findViewById(R.id.switch1);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request missing location permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_CAMERA);
+        } else {
+            swc.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
+                    String str = String.valueOf(isChecking); // boolean -> String 변환
+
+                    // 상태가 on, off인 경우에 알맞게 토스트를 띄움
+                    if (isChecking) {
+                        mCamera = Camera.open();
+                        Camera.Parameters mCameraParameters = mCamera.getParameters();
+                        mCameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        mCameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+                        mCamera.setParameters(mCameraParameters);
+                        mCamera.startPreview();
+                        isCameraOn = true;
+                        swc2.setVisibility(View.VISIBLE);
+                        textView.setVisibility(View.VISIBLE);
+                    } else {
+                        mCamera.release();
+                        isCameraOn = false;
+
+                    }
+                }
+            });
+            swc2 = (Switch)findViewById(R.id.switch2);
+            swc2.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton cb, boolean isChecking2) {
+                    String str2 = String.valueOf(isChecking2); // boolean -> String 변환
+
+                    // 상태가 on, off인 경우에 알맞게 토스트를 띄움
+                    if(isChecking2){
+                        mCamera = Camera.open();
+                        Camera.Parameters mCameraParameters = mCamera.getParameters();
+                        mCameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+                        mCameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+                        mCamera.setParameters(mCameraParameters);
+                        mCamera.startPreview();
+                        isCameraOn = true;}
+                    else{
+                        mCamera.release();
+                        isCameraOn = false;}
+                }
+            });
+        }
+
         swc.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton cb, boolean isChecking) {
                 String str = String.valueOf(isChecking); // boolean -> String 변환
@@ -88,7 +142,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 
 }
